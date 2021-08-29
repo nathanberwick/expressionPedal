717f61b1549222236710af954aa0a1e12f099d32
Nathan Berwick <72820777+nathanberwick@users.noreply.github.com>
2021-08-29 15:02:01 +0200

    Add files via upload

--- expresionPedal.ino
+++ expresionPedal.ino
@@ -7,14 +7,14 @@
 #include "vector"
 #include "OutputPinClass.h"
 
-int m_expressionReadPin = 0;
-int m_smoothingReadPin = 1;
-std::vector<outputPin> m_outputPins(8);
+int m_expressionReadPin = A0;
+int m_smoothingReadPin = A1;
+std::vector<OutputPinClass> m_outputPins(5);
 float m_currentInputValue = 0;
 float m_scaledSmoothingValue = 0;
 float m_directSmoothingValue = 0;
 bool m_multiControl = false; //not happy with this name.
-int m_multiControlPin = 0;
+int m_multiControlPin = 2; //digital 2
 float m_smoothingMsMin = 2.0f;
 float m_smoothingMsMax = 100.0f;
 
@@ -38,23 +38,23 @@ float interpolate(float oldVal, float newVal, float smoothingVal)
 void setup() {
   //TODO: check state of unused pins on default could just set to zero here.
   //TODO: possible to save last used state before off?
-  m_outputPins[0].setPinNumber(0);
-  m_outputPins[1].setPinNumber(1);
-  m_outputPins[2].setPinNumber(2);
-  m_outputPins[3].setPinNumber(3);
-  m_outputPins[4].setPinNumber(4);
-  m_outputPins[5].setPinNumber(5);
-  m_outputPins[6].setPinNumber(6);
-  m_outputPins[7].setPinNumber(7);
+  m_outputPins[0].setPinNumber(3);
+  m_outputPins[1].setPinNumber(5);
+  m_outputPins[2].setPinNumber(6);
+  m_outputPins[3].setPinNumber(9);
+  m_outputPins[4].setPinNumber(10);
 
-  m_outputPins[0].setLedPinNumber(8);
-  m_outputPins[1].setLedPinNumber(9);
-  m_outputPins[2].setLedPinNumber(10);
+  m_outputPins[0].setLedPinNumber(4);
+  m_outputPins[1].setLedPinNumber(7);
+  m_outputPins[2].setLedPinNumber(8);
   m_outputPins[3].setLedPinNumber(11);
   m_outputPins[4].setLedPinNumber(12);
-  m_outputPins[5].setLedPinNumber(13);
-  m_outputPins[6].setLedPinNumber(14);
-  m_outputPins[7].setLedPinNumber(15);
+
+  m_outputPins[0].setSwitchNumber(A2);
+  m_outputPins[1].setSwitchNumber(A3);
+  m_outputPins[2].setSwitchNumber(A4);
+  m_outputPins[3].setSwitchNumber(A5);
+  m_outputPins[4].setSwitchNumber(A6);
 
   m_outputPins[0].setState(true);
   digitalWrite(m_outputPins[0].getLedPinNumber(), m_outputPins[0].getState());
@@ -68,7 +68,7 @@ void loop() {
   //update buttons pressed
   for (auto iter = m_outputPins.begin(); iter != m_outputPins.end(); ++iter)
   {
-    bool readIn = digitalRead(iter->getPinNumber());
+    bool readIn = digitalRead(iter->getSwitchNumber());
 
     if (readIn == iter->getSwitchState())
       continue;
